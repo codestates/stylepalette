d16a8eddedcfb6ea2135b41cea1b7781068021c3
nayeonseo <nayeon.e.seo@gmail.com>
2021-08-08 23:57:10 -0700

    Add login modal

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,49 +1,58 @@
-import React from "react";
-import styled from "styled-components";
-import { PrimaryButton, SecondaryButton, DefaultButton } from "./Button.styled";
+import React from 'react';
+import styled from 'styled-components';
+import {
+    PrimaryButton,
+    SecondaryButton,
+    DefaultButton,
+    CloseButton,
+} from './Button.styled';
 
 interface ButtonProps {
-  primary?: string;
-  secondary?: string;
-  children?: any;
+    primary?: boolean;
+    secondary?: boolean;
+    close?: boolean;
+    onClick?: () => void;
+    children?: any;
 }
 
 export default function Button(props: ButtonProps) {
-  if (props.primary) {
-    return <PrimaryButton>{props.children}</PrimaryButton>;
-  } else if (props.secondary) {
-    return <SecondaryButton>{props.children}</SecondaryButton>;
-  }
-  return <DefaultButton>{props.children}</DefaultButton>;
+    if (props.primary) {
+        return <PrimaryButton>{props.children}</PrimaryButton>;
+    } else if (props.secondary) {
+        return <SecondaryButton>{props.children}</SecondaryButton>;
+    } else if (props.close) {
+        return <CloseButton>{props.children}</CloseButton>;
+    }
+    return <DefaultButton>{props.children}</DefaultButton>;
 }
 
 export const StyledButton = styled.button<ButtonProps>`
-  background: transparent;
-  color: palevioletred;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  margin: 0.5em 1em;
-  padding: 0.25em 1em;
+    background: transparent;
+    color: palevioletred;
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    margin: 0.5em 1em;
+    padding: 0.25em 1em;
 
-  ${(props) => {
-    if (props.primary) {
-      return `
+    ${(props) => {
+        if (props.primary) {
+            return `
         background: palevioletred;
         color: white;
     `;
-    } else if (props.secondary) {
-      return `
+        } else if (props.secondary) {
+            return `
         background: skyblue;
         color: black;
     `;
+        }
+    }}
+
+    &:disabled {
+        // change opacity
     }
-  }}
 
-  &:disabled {
-    // change opacity
-  }
-
-  &::hover {
-    // change color
-  }
+    &::hover {
+        // change color
+    }
 `;
