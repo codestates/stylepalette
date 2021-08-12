6ff01b4a4315f7bfaaa1da05970808bd7b22dd20
nayeonseo <nayeon.e.seo@gmail.com>
2021-08-11 21:01:57 -0700

    Add mobile header

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Text from './components/Text/Text';
 import Button from './components/Button/Button';
 import StyledText from './components/Text/Text';
 import Header from './components/Header/Header';
-
 import GenderSelect from './pages/GenderSelect';
 
 const GlobalStyle = createGlobalStyle`
@@ -24,20 +23,6 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Text size="large" />
-            <header className="App-header">
-              <StyledText size="small">
-                Edit <code>src/App.js</code> and save to reload.
-              </StyledText>
-              <Button primary>Hello</Button>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
           </Route>
           <Route exact path="/mypage">
             mypage
