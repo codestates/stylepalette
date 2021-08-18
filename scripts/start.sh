17600cefdcd633c8a40de86a25b62e554db3590d
KyuhyunKim <ggh163700@gmail.com>
2021-08-18 10:35:46 +0900

    fix:script file

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,11 @@ export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE --query Parameters[0].Value | sed 's/"//g')
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 
+cd /home/ubuntu/stylepalette/server/src/
+
+tsc
+
 cd /home/ubuntu/stylepalette/server/src/build
+
 authbind --deep pm2 stop index.js
 authbind --deep pm2 start index.js
