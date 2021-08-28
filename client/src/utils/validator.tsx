55f4a9068caa0d33e570dafa7cc96a0596516daa
nayeonseo <nayeon.e.seo@gmail.com>
2021-08-27 22:51:28 -0700

    Validation

--- client/src/utils/validator.tsx
+++ client/src/utils/validator.tsx
@@ -1,6 +1,6 @@
 // 아이디: 영어 또는 숫자만 가능
 export function validId(str: string) {
-  return /^[A-Za-z][A-Za-z0-9]*$/.test(str);
+  return /^[A-Za-z][A-Za-z0-9]{4,15}$/.test(str);
 }
 
 // 비밀번호: 최소 8자 이상 15자이면서, 알파벳과 숫자 및 특수문자(@$!%*#?&) 는 하나 이상 포함
